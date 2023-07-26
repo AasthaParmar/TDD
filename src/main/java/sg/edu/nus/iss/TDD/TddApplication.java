42692a6e067c2b99db866784d520c7e4ccb2a7e2
Darryl <darryl1975@hotmail.com>
2023-07-26 15:55:57 +0800

    added a comment in TddApplication.java

--- src/main/java/sg/edu/nus/iss/TDD/TddApplication.java
+++ src/main/java/sg/edu/nus/iss/TDD/TddApplication.java
@@ -3,6 +3,8 @@ package sg.edu.nus.iss.TDD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Testing CodeQL
+
 @SpringBootApplication
 public class TddApplication {
 
